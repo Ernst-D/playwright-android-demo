0b6d8543c982808aa3e1267c29e29bd4b6b85ac6
Ernst-D <ernst.dzerevanko@gmail.com>
2022-12-25 00:26:40 +0400

    recording traces, increasing timeouts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,13 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+    timeout: 60000 * 30,
     use: {
         headless: false,
         viewport: { width: 1280, height: 720 },
         ignoreHTTPSErrors: true,
         video: 'on-first-retry',
+        trace:"on",
     },
 };
 export default config;
